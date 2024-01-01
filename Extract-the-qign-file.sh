0d0ecdcc0e91f35d59af202e63dfe44bf82a89ed
xiaotian2333 <1719549416@qq.com>
2024-01-01 21:40:23 +0800

    将文件改为云端获取

--- Extract-the-qign-file.sh
+++ Extract-the-qign-file.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+# 获取云端文件
+wget https://oss.xt-url.com/sign.zip
+
 # 解压文件
 unzip sign.zip
 
 # 输出各目录下的文件
+echo -e "\e[1;36m===============\e[0m"
 echo -e "\e[1;36m主目录文件\e[0m"
 ls
-
+echo -e "\e[1;36m===============\e[0m"
 echo -e "\e[1;36mbin目录文件\e[0m"
 cd bin && ls && cd ..
-
+echo -e "\e[1;36m===============\e[0m"
 echo -e "\e[1;36mlib目录文件\e[0m"
 cd lib && ls && cd ..
-
+echo -e "\e[1;36m===============\e[0m"
 echo -e "\e[1;36mtxlib目录文件\e[0m"
 cd txlib && ls && cd ..
-
+echo -e "\e[1;36m===============\e[0m"
 # 启动
 bash app.sh
