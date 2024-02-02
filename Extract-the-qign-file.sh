b736140fe4e19dc142270688199a8b63d625f8b0
xiaotian2333 <1719549416@qq.com>
2024-02-02 16:05:16 +0800

    更新启动文件

--- Extract-the-qign-file.sh
+++ Extract-the-qign-file.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
 # 获取云端文件
-wget https://oss.xt-url.com/sign.zip
+wget -O QSign.zip 'https://www.yuanxiapi.cn/api/lanzou/?url=https://overtimebunny.lanzoul.com/QSignAMD&type=down'
 
 # 解压文件
-unzip sign.zip
+unzip QSign.zip
 
 # 输出各目录下的文件
 echo -e "\e[1;36m===============\e[0m"
 echo -e "\e[1;36m主目录文件\e[0m"
 ls
 echo -e "\e[1;36m===============\e[0m"
-echo -e "\e[1;36mbin目录文件\e[0m"
-cd bin && ls && cd ..
-echo -e "\e[1;36m===============\e[0m"
-echo -e "\e[1;36mlib目录文件\e[0m"
-cd lib && ls && cd ..
-echo -e "\e[1;36m===============\e[0m"
-echo -e "\e[1;36mtxlib目录文件\e[0m"
-cd txlib && ls && cd ..
-echo -e "\e[1;36m===============\e[0m"
+
 # 启动
-bash app.sh
+bash X
 exit
